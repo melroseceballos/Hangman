f0e52d860fc4512931a4c97eacfea08dcfa354e9
Melrose Ceballos <melroseceballos@Melroses-MBP.lan>
2023-02-14 18:00:37 -0800

    Basic intro design done

---
+++ browser-game/app.js
@@ -0,0 +1,28 @@
+//Change play button color during hover
+document.getElementById("play").onmouseover = function () {
+    this.style.color = green;
+}
+
+
+// Create an array of animals for the words
+
+//use play button to generate frame 2?
+// use prompt sync for user input for the letter
+
+// use new button to generate a new word using math.random
+
+/**** When a user gets a wrong answer*********
+ * show lines when user gets it wrong
+ * 
+ * toggle class names for lines to display dependent on answer
+ * 
+ * 
+ */
+
+/* When user gets right answer ****************
+* when a user gets a letter right put right letter on lines
+*
+*
+*
+*
+**/
