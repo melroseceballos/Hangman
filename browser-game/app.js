16dddd36b29f0de34bd8c29e1b0f64f91c00e88c
Melrose Ceballos <melroseceballos@Melroses-MBP.lan>
2023-02-16 00:11:35 -0800

    Added letter buttons

--- browser-game/app.js
+++ browser-game/app.js
@@ -1,10 +1,26 @@
 //const prompt = required('prompt-sync') ();
 
-var animal = ["cat", "dog", "rat", "tiger", "wolf", "deer", "panda","elephant", "buffalo", "tortoise", "penguin", "alligator", "badger","snake", "armadillo", "axolotl", "octopus", "whale", "moles", "goose", "cheetah", "chipmunk", "gecko", "sea lion", "gorilla" , "magpie", "antelope", "rabbits" ];
-var word = animal[Math.floor(Math.random () * animal.length)]
+//Word Sequence
+/**const animal = "animals" = ["cat", "dog", "rat", "tiger", "wolf", "deer", "panda","elephant", "buffalo", "tortoise", "penguin", "alligator", "badger","snake", "armadillo", "axolotl", "octopus", "whale", "moles", "goose", "cheetah", "chipmunk", "gecko", "sea lion", "gorilla" , "magpie", "antelope", "rabbits" ];
+var words = animal[Math.floor(Math.random () * animal.length)]
 
-console.log(word);
-//when user clicks play display 2nd wireframe
+var word = words [Math.floor(Math.random () * words.length)];
+var answerArray = [];
+for(var i = 0; i<word.length; i++) {
+    answerArray [i] = "_";
+
+    var remainingLetter = word.length;
+}*/ 
+
+
+//play button sequence for window 2
+function hide (){
+    document.getElementById('welcome').style.display = 'none';
+    document.getElementById('play').style.display = 'none';
+}
+
+
+
 // use prompt sync for user input for the letter
 
 //ask user for  a new game?
