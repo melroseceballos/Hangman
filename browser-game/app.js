78ea4c09b4682c0cdeffa2ce9180943064e18572
Melrose Ceballos <melroseceballos@Melroses-MBP.lan>
2023-02-16 09:01:01 -0800

    Figuring out how to generate word and disable buttons after use

--- browser-game/app.js
+++ browser-game/app.js
@@ -4,37 +4,41 @@ function hide (){
     document.getElementById('play').style.display = 'none';
     document.getElementById('word-guess').style.display = "block";
     document.getElementById('letters').style.display = "block";
-    document.getElementById('mystery-word').style.display = "block";
+//figure out how to show the gallows when play is clicked
 }
 
 //GUESS WORD GENERATOR
+const guessWord = document.getElementById('word-guess');
+const letterBtns = document.querySelectorAll('#letters button');
 const words = ["cat", "dog", "rat", "tiger", "wolf", "deer", "panda","elephant", "buffalo", "tortoise", "penguin", "alligator", "badger","snake", "armadillo", "axolotl", "octopus", "whale", "moles", "goose", "cheetah", "chipmunk", "gecko", "sea lion", "gorilla" , "magpie", "antelope", "rabbits" ];
  let wordIndex = Math.floor(Math.random() * words.length);
  magicWord = words[wordIndex];
 console.log(magicWord)
-// use prompt sync for user input for the letter
 
-//ask user for  a new game?
-// use new button to generate a new word using math.random
-
-//make a div for second frame
-//event listener when play is clicked second frame div is shown
-
-
-/**** When a user gets a wrong answer*********
- * show lines when user gets it wrong
- * 
- * toggle class names for lines to display dependent on answer
- * 
- * 
- */
-
-/* When user gets right answer ****************
-* when a user gets a letter right put right letter on lines
-*
-*
-*
-*
-**/
-
-//have a  button for user to terminate before game or during game
+//GAMEPLAY
+let WrongCount = 6;
+document.getElementById('letters')
+  .addEventListener('click', handleLetterClick);
+  function handleLetterClick(evt) {
+    let letter = evt.target.textContent;
+    if (
+      evt.target.tagName !== 'BUTTON' ||
+      usedLetters.includes(letter) ||
+      secretWord === guessWord ||
+      wrongLetters.length === LOSE_WRONG_COUNT
+    ) return;
+    usedLetters.push(letter);
+    if (secretWord.includes(letter)) {
+      // correct guess
+      let newGuessWord = '';
+      for (let i = 0; i < secretWord.length; i++) {
+        newGuessWord += (secretWord.charAt(i) === letter) ? letter : guessWord.charAt(i);
+      }
+      guessWord = newGuessWord;
+    } else {
+      wrongLetters.push(letter);
+    }
+    render();
+}
+//figure out how to generate word and use "guess-word" as placement holder
+//figure out to disable buttons when used
